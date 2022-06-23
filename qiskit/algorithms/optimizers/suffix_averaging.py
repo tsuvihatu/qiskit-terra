02aac1993e9867b2a636783ee74b98e84ec399c9
Shiro Tamiya <shirotamiya@ShironoMacBook-Pro-2.local>
2022-06-23 16:14:11 +0900

    Inherited from optimizer class.

--- qiskit/algorithms/optimizers/suffix_averaging.py
+++ qiskit/algorithms/optimizers/suffix_averaging.py
@@ -2,17 +2,16 @@ from json import load
 import os
 import csv
 import numpy as np
-from gc import callbacks
-from typing import Any, Callable, Dict, Union, List, Optional, Tuple
+from qiskit.algorithms.optimizers import QNSPSA, SPSA, NFT, GradientDescent
+from typing import Callable, List, Optional, Tuple
 
 from scipy_optimizer import SciPyOptimizer
-from optimizer import Optimizer, OptimizerSupportLevel, OptimizerResult, POINT
+from optimizer import Optimizer, OptimizerResult, POINT, OptimizerSupportLevel
 
-class SuffixAveragingOptimizer(SciPyOptimizer):
+class SuffixAveragingOptimizer(Optimizer):
     def __init__(
         self,
-        optimizer: SciPyOptimizer,
-        optimizer_name: str, 
+        optimizer: Optimizer,
         alpha: float = 0.3,
         suffix_dir: str = None,
         suffix_filename: str = None,
@@ -21,7 +20,7 @@ class SuffixAveragingOptimizer(SciPyOptimizer):
     ) -> None:
         """
         Args:
-            optimizer: 
+            optimizer: The optimizer used for optimizing parameterized quantum circuits.
             alpha: The hyperparameter to determine the ratio of ansatz parameters to take the average.
             suffix_dir: The directory for storing ansatz parameters.
             suffix_filename: The filename for storing ansatz parameters.
@@ -43,32 +42,40 @@ class SuffixAveragingOptimizer(SciPyOptimizer):
 
         self._circ_params = []
         
-        if optimizer_name == "SPSA":
+        if isinstance(self._optimizer, SPSA):
             def load_params(nfev, x_next, fx_next, update_step, bool):
                 self._circ_params.append(x_next)
-        elif optimizer_name == "QNSPSA":
+        elif isinstance(self._optimizer, QNSPSA):
             def load_params(nfev, x_next, fx_next, update_step, bool):
                 self._circ_params.append(x_next)
-        elif optimizer_name == "GradientDescent":
+        elif isinstance(self._optimizer, GradientDescent):
             def load_params(nfevs, x_next, fx_next, stepsize):
                 self._circ_params.append(x_next)
         else:
             def load_params(x):
                 self._circ_params.append(x)
 
-        #super().__init__(method=optimizer._method, options=optimizer._options, callback=load_params)
         self._optimizer.__init__(callback = load_params)
 
     def _save_circ_params(self, circ_params: List[float], csv_dir: str, csv_filename: str) -> None:
-        with open(os.path.join(csv_dir, csv_filename+".csv"), mode="a") as csv_file:
+        with open(os.path.join(csv_dir, csv_filename+".csv"), mode="w") as csv_file:
             writer = csv.writer(csv_file, lineterminator='\n')
             writer.writerows(circ_params)
 
-    def read_circ_params(self, csv_dir: str, csv_filename: str) -> List[float]:
+    @staticmethod
+    def read_circ_params(csv_dir: str, csv_filename: str) -> List[float]:
         with open(os.path.join(csv_dir, csv_filename+".csv")) as csv_file:
             reader = csv.reader(csv_file)
             circ_params = [list(map(float, row)) for row in reader]
         return circ_params
+
+    def get_support_level(self):
+        """Return support level dictionary"""
+        return {
+            "gradient": OptimizerSupportLevel.supported,
+            "bounds": OptimizerSupportLevel.ignored,
+            "initial_point": OptimizerSupportLevel.supported,
+        }
 
     def _return_suffix_average(self) -> List[float]:
         if self._save_params:
@@ -83,15 +90,24 @@ class SuffixAveragingOptimizer(SciPyOptimizer):
                     averaged_param += self._circ_params[i-j]
                 averaged_param /= np.ceil(i*self._alpha)
                 averaged_params[i] = averaged_param
-            self._save_circ_params(averaged_params, self._suffix_dir, self._suffix_filename+"suffix")
+            self._save_circ_params(averaged_params, self._suffix_dir, self._suffix_filename+"_suffix")
             return averaged_params[-1]
                 
         else:
-            n_iterates = len(self._circ_params)
-            averaged_param = np.zeros_like(self._circ_params[0])
-            for j in range(int(np.ceil(n_iterates*self._alpha))):
-                averaged_param += self._circ_params[n_iterates-j-1]
-            averaged_param /= np.ceil(n_iterates*self._alpha)
+            if isinstance(self._optimizer, NFT):
+                n_params = int(len(self._circ_params[0]))
+                n_iterates = int(len(self._circ_params))
+                n_repitition = int(len(self._circ_params)/n_params)
+                averaged_param = np.zeros_like(self._circ_params[0])
+                for j in range(int(np.ceil(n_repitition*self._alpha))):
+                    averaged_param += self._circ_params[n_iterates-n_params*j-1]
+                averaged_param /= np.ceil(n_repitition*self._alpha)
+            else:
+                n_iterates = len(self._circ_params)
+                averaged_param = np.zeros_like(self._circ_params[0])
+                for j in range(int(np.ceil(n_iterates*self._alpha))):
+                    averaged_param += self._circ_params[n_iterates-j-1]
+                averaged_param /= np.ceil(n_iterates*self._alpha)
 
             return averaged_param
         
